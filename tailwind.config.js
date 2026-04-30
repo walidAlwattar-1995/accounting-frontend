4d5e0cf322f01224ea0e20b94058457c7cc5bbc6
walidAlwattar-1995 <walidalwattar1995@gmail.com>
2026-04-30 17:32:50 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#1e40af',
+          dark: '#1e3a8a',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
